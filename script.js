fb72917a41c151a47c6ae9d5d4610a2910b0ebed
Pranjal Sharma <spranjal0515@gmail.com>
2023-08-24 23:23:13 +0530

    done

---
+++ script.js
@@ -0,0 +1,37 @@
+const addTitle = document.getElementById('addTitle');
+const addText = document.getElementById('addText');
+const addNoteButton = document.getElementById('addNote');
+const notesDiv = document.getElementById('notes');
+let notes=[]; 
+
+function addNotes(){
+    
+
+    if(addText.value == ''){
+        alert('Add your note');
+        return;
+    }
+    
+    const noteObj = {
+        title: addTitle.value,
+        text: addText.value,
+    }
+
+    notes.push(noteObj);
+    showNotes();
+}
+
+function showNotes(){
+    let notesHTML = '';
+    
+    for(let i=0; i<notes.length; i++){
+        notesHTML += `<div class="note">
+                    <button>Delete</button>
+                    <div class="title">${notes[i].title === "" ? 'Note' : notes[i].title}</div>
+                    <div class="text">${notes[i].text}</div>
+                </div>
+        `
+    }
+    notesDiv.innerHTML = notesHTML;
+}
+addNoteButton.addEventListener('click',addNotes);
